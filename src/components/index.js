e3cc52fccefae7317fe255b3cb7e4c4d88ee7d39
unknown <manishsinghdangi638@gmail.com>
2024-09-15 16:12:40 +0530

    Second

--- src/components/index.js
+++ src/components/index.js
@@ -1,14 +1,14 @@
 export {default as Navbar} from "./Navbar"
-export {default as Feed} from "./Feed"
-export {default as VideoDetail} from "./VideoDetail"
-export {default as ChannelDetail} from "./ChannelDetail"
-export {default as SearchFeed} from "./SearchFeed"
+export {default as Feed} from "./Feed.jsx"
+export {default as VideoDetail} from "./VideoDetail.jsx"
+export {default as ChannelDetail} from "./ChannelDetail.jsx"
+export {default as SearchFeed} from "./SearchFeed.jsx"
 export {default as SearchBar} from "./SearchBar"
 export {default as Sidebar} from "./SideBar"
 export {default as Videos} from "./Videos"
-export {default as VideoCard} from "./VideoCard"
-export { default as Loader } from './Loader'
-export { default as ChannelCard } from './ChannelCard'
+export {default as VideoCard} from "./VideoCard.jsx"
+export { default as Loader } from './Loader.jsx'
+export { default as ChannelCard } from './ChannelCard.jsx'
 
 
 
